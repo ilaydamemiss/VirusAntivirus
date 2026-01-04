79594f13d3b81248f82c16d823bd27b0df2cfcb9
ilaydamemiss <oozgememis@gmail.com>
2026-01-04 19:33:35 +0300

    İlk commit - VirusAntivirus projesi

---
+++ AntiVirus/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.IO;
+
+namespace Antivirus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antivirüs Taraması Başlıyor...");
+
+            // Antivirüs de Masaüstüne bakacak
+            string taranacakYol = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string taranacakVirus = "VIRUS_KODU_X99";
+
+            // Masaüstündeki tüm .txt dosyalarını bul
+            string[] dosyalar = Directory.GetFiles(taranacakYol, "*.txt");
+            int bulunan = 0;
+
+            foreach (string dosya in dosyalar)
+            {
+                string icerik = File.ReadAllText(dosya);
+
+                if (icerik.Contains(taranacakVirus))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"VİRÜS BULUNDU: {Path.GetFileName(dosya)}");
+
+                    File.Delete(dosya); // SİLME İŞLEMİ
+
+                    Console.WriteLine("-> Dosya imha edildi.");
+                    Console.ResetColor();
+                    bulunan++;
+                }
+            }
+
+            if (bulunan == 0)
+                Console.WriteLine("Masaüstünde virüs bulunamadı.");
+            else
+                Console.WriteLine($"\nTarama bitti. {bulunan} virüs temizlendi.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VirusOdev/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace VirusYapici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dosyayı direkt Masaüstüne oluşturacağız ki hoca görsün
+            string masaustuYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string dosyaAdi = "tehlikeli_dosya.txt";
+            string tamYol = Path.Combine(masaustuYolu, dosyaAdi);
+
+            string virusKodu = "VIRUS_KODU_X99";
+
+            Console.WriteLine("Virüs Programı Çalıştı...");
+
+            try
+            {
+                // Masaüstüne virüslü dosyayı bırak
+                File.WriteAllText(tamYol, "Bu dosyanın içinde gizli kod var.\n" + virusKodu);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Masaüstüne virüslü dosya bırakıldı!");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+
+            Console.WriteLine("\nKapatmak için Enter'a bas.");
+            Console.ReadLine();
+        }
+    }
+}
